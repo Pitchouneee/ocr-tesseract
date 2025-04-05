96e7ad90c9f7d317ad229584401019be76e5689c
Corentin Bringer <corentinbringer@ik.me>
2025-04-05 11:53:30 +0200

    init

---
+++ src/main/java/fr/corentinbringer/ocr_tesseract/OcrTesseractApplication.java
@@ -0,0 +1,13 @@
+package fr.corentinbringer.ocr_tesseract;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OcrTesseractApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OcrTesseractApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/corentinbringer/ocr_tesseract/OcrTesseractApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.corentinbringer.ocr_tesseract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OcrTesseractApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
